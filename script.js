e52ee1aa5a973b9f2fd88cbb59f88344013e1e89
Samed Shukurov <samedzauroglu2007@gmail.com>
2023-11-07 12:47:25 +0400

    First Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const dataTable = document.querySelector(".data");
+
+fetch("db.json")
+    .then((res) => res.json())
+    .then((data) =>
+        data.map((item) => {
+            dataTable.innerHTML += `
+                            <li>
+        <a
+            title="download"
+            href="${item.url}"
+            download
+            >${item.name}</a
+        
+        >
+    </li>`;
+        })
+    );
